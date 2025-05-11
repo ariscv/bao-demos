1ca2314306ab7b761dc5b1ec74dbfb2f4afd0a60
msuad <1445617868@qq.com>
2025-05-11 21:13:56 +0800

    初步的修改

--- guests/baremetal/make.mk
+++ guests/baremetal/make.mk
@@ -1,5 +1,5 @@
 baremetal_src:=$(wrkdir_src)/baremetal
-baremetal_repo:=https://github.com/bao-project/bao-baremetal-guest.git 
+baremetal_repo:=https://github.com/ariscv/bao-baremetal-guest.git 
 baremetal_branch:=demo
 
 $(baremetal_src):
